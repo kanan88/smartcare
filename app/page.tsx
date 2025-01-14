b2db6de6706ed4b24049c15f5589213b2adcda1e
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-14 02:32:35 +0100

    integrate project specific theme, install shadcn, add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <h1 className="text-3xl underline">Home</h1>;
+  return (
+    <>
+      <h1 className="text-white text-3xl underline">Home page</h1>
+      <Button>Click me</Button>
+    </>
+  );
 };
 
 export default HomePage;
